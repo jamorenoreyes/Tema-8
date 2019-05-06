d27e8fa4f7b7bc8d690b2121b541d2b0f0f56c77
Juanan <Juanan@Juanan-PC>
2019-05-06 10:10:00 +0200

    pruebas unitarias

--- test/ejerfiprueuni/Parametrizado.java
+++ test/ejerfiprueuni/Parametrizado.java
@@ -11,7 +11,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.runners.Parameterized;
 
 /**
@@ -23,8 +23,27 @@ public class Parametrizado {
     public static Iterable data(){
         return Arrays.asList(new Object[][]{{1,"JFJ","JJ","Alicante","elche","calle"},{2,"Antonio","JcccJ","Alicante","Alicane","Espronceda"},{3,"Luis","JcrgtyeccJ","Alicante","Santa Pola","Pedro Juan"},{6,"Leopoldo","Vende_todo","Madrid","Alconbendas","Avda.Libertad"}});
         }
+    private int numvend;
+    private String nomvend,nombrecomer,provincia,localidad,direccion;
     
-    public Parametrizado() {
+    public Parametrizado(int numvend,String nomvend,String nombrecomer,String provincia,String localidad,String direccion) {
+    this.numvend=numvend;
+    this.nomvend.equals(nomvend);
+    this.nombrecomer.equals(nombrecomer);
+    this.provincia.equals(provincia);
+    this.localidad.equals(localidad);
+    this.direccion.equals(direccion);
+    }
+    @Test
+    
+    public void Eliminar_vendedor(){
+     //  Vendedor.             // tank.fill(relleno);
+        //double nivel=tank.getTankLevel();
+        //Assert.assertEquals(esperado,nivel,0.0);
+    }
+    
+    public void Buscar_Vendedor(){
+       // Assert.assertArrayEquals(expecteds, actuals);
     }
     
     @BeforeClass
