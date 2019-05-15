abb2593dff20217fd990e06409a14a24c50a6d0f
Juanan <Juanan@Juanan-PC>
2019-05-15 09:10:24 +0200

    Vendedores

--- test/ejerfiprueuni/Parametrizado.java
+++ test/ejerfiprueuni/Parametrizado.java
@@ -16,33 +16,27 @@ import org.junit.runners.Parameterized;
  */
 @RunWith(Parameterized.class)
 public class Parametrizado {
-    public ArrayList<Vendedor> todosVendedores=new ArrayList<Vendedor>();
-    static Vendedores vendeProv=new Vendedores(); 
+    public Vendedores todosVendedores;
+    static   ArrayList <Vendedor>  VendedoresAlicante; 
+    String provincia;
     @Parameterized.Parameters
     public static Iterable data(){
-    return Arrays.asList(new Object[][]{{"Alicante",vendeProv}});
+    return Arrays.asList(new Object[][]{{"Alicante",VendedoresAlicante}});
     }
     private int numvend;
-    private String nomvend,nombrecomer,provincia,localidad,direccion;
     
-    public Parametrizado(int numvend,String nomvend,String nombrecomer,String provincia,String localidad,String direccion) {
-    this.numvend=numvend;
-    this.nomvend.equals(nomvend);
-    this.nombrecomer.equals(nombrecomer);
-    this.provincia.equals(provincia);
-    this.localidad.equals(localidad);
-    this.direccion.equals(direccion);
+    
+    public Parametrizado(String provincia, ArrayList <Vendedor>  VendedoresAlicante) {
+            this.provincia=provincia;
     }
     @Test
     public void Eliminar_vendedor(){
-      vendeProv.eliminar_Vendedor(numvend);    
-        //double nivel=tank.getTankLevel();
-        //Assert.assertEquals(esperado,nivel,0.0);
+     VendedoresAlicante.remove(numvend);    
     }
     
     public void Buscar_Vendedor(){
-      todosVendedores v=vendeProv.buscar_Vendedor(provincia); 
-      Assert.assertArrayEquals(numvend,todosVendedores);
+      ArrayList esperado= todosVendedores.buscar_Vendedor(provincia); 
+      Assert.assertEquals(VendedoresAlicante,esperado);
     }
     
     @BeforeClass
@@ -55,25 +49,25 @@ public class Parametrizado {
     
     @Before
     public void setUp() {
-    Vendedor v1=new Vendedor(1,"Pedro","Vende_todo",provincia,"elche","Avda_Libertad");
-    Vendedor v2=new Vendedor(2,"Antonio","Numero_uno",provincia,"Alicane","Espronceda");
-    Vendedor v3=new Vendedor(3,"Luis","Charlatan",provincia,"Santa Pola","Pedro Juan");
-    Vendedor v4=new Vendedor(4,"Paco","El_rapido",provincia,"Sant","Torres_Quevedo");
+    Vendedor v1=new Vendedor(1,"Pedro","Vende_todo","Alicante","elche","Avda_Libertad");
+    Vendedor v2=new Vendedor(2,"Antonio","Numero_uno", "Valencia","Alicane","Espronceda");
+    Vendedor v3=new Vendedor(3,"Luis","Charlatan","Alicante","Santa Pola","Pedro Juan");
+    Vendedor v4=new Vendedor(4,"Paco","El_rapido","Alicante","Santa Pola","Torres_Quevedo");
     Vendedor v5=new Vendedor(5,"luis","Come_bolas","Valencia","Torrent","Calle_grande");
-    todosVendedores.add(v1);
-    todosVendedores.add(v2);
-    todosVendedores.add(v3);
-    todosVendedores.add(v4);
-    todosVendedores.add(v5);
+    todosVendedores.anyadir_Vendedor(v1);
+    todosVendedores.anyadir_Vendedor(v2);
+    todosVendedores.anyadir_Vendedor(v3);
+    todosVendedores.anyadir_Vendedor(v4);
+    todosVendedores.anyadir_Vendedor(v5);
+    VendedoresAlicante.add(v1);
+    VendedoresAlicante.add(v3);
+    VendedoresAlicante.add(v4);
+    
     }
     
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+  
 }
